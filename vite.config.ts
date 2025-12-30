1233baa499d484a0b79fa70f002bdd1876187726
kevinccbsg <kevinccbsg@gmail.com>
2025-12-30 21:34:20 +0100

    ci: add coverage for frontend app

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// add plugin for code coverage
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +14,13 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+    // configure istanbul plugin
+    istanbul({
+      include: 'src/**/*',
+      exclude: ['node_modules', 'tests/'],
+      extension: ['.ts', '.tsx'],
+      requireEnv: true,
+    }),
   ],
   server: {
     port: 5173,
